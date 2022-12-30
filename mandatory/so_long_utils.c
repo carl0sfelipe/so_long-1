51973bc01c66b8343dbd11470673c894eaf9b889
Carlos Macbook Pro <carlosmac@Carloss-MacBook-Pro.local>
2022-12-29 22:25:18 -0300

    normie-utils

--- mandatory/so_long_utils.c
+++ mandatory/so_long_utils.c
@@ -1,37 +1,38 @@
 /* ************************************************************************** */
-/*                                                                            */
-/*                                                        :::      ::::::::   */
+/*	  */
+/*	::: ::::::::   */
 /*   so_long_utils.c                                    :+:      :+:    :+:   */
-/*                                                    +:+ +:+         +:+     */
-/*   By: csiqueir <csiqueir@student.42.fr>          +#+  +:+       +#+        */
-/*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2022/12/28 23:13:56 by csiqueir          #+#    #+#             */
-/*   Updated: 2022/12/29 21:53:19 by csiqueir         ###   ########.fr       */
-/*                                                                            */
+/*	+:+ +:+ +:+   */
+/*   By: csiqueir <csiqueir@student.42.fr>  +#+  +:+	 +#+  */
+/*	+#+#+#+#+#+   +#+  */
+/*   Created: 2022/12/28 23:13:56 by csiqueir	#+#  #+#  */
+/*   Updated: 2022/12/29 22:10:52 by csiqueir         ###   ########.fr       */
+/*	  */
 /* ************************************************************************** */
 
 #include "so_long.h"
 
-void put_exit_img(t_init_map *data, int *j, int *i)
+void	put_exit_img(t_init_map *data, int *j, int *i)
 {
-mlx_put_image_to_window(data->mlx, data->win, data->graph->exit, *j * 40, *i * 40);
+	mlx_put_image_to_window(data->mlx, data->win,
+		data->graph->exit, *j * 40, *i * 40);
 }
 
-void put_collect_img(t_init_map *data, int *j, int *i)
+void	put_collect_img(t_init_map *data, int *j, int *i)
 {
-mlx_put_image_to_window(data->mlx, data->win, data->graph->collect, *j * 40, *i * 40);
+	mlx_put_image_to_window(data->mlx, data->win,
+		data->graph->collect, *j * 40, *i * 40);
 }
 
-void ft_process_char(t_init_map *data, int *ex, char c)
+void	ft_process_char(t_init_map *data, int *ex, char c)
 {
-    if (c == 'P')
-        data->player += 1;
-    else if (c == 'E')
-        (*ex)++;
-    else if (c == 'C')
-        data->count += 1;
+	if (c == 'P')
+		data->player += 1;
+	else if (c == 'E')
+		(*ex)++;
+	else if (c == 'C')
+		data->count += 1;
 }
-
 
 void	ft_game_result(t_init_map *data)
 {
@@ -40,20 +41,25 @@ void	ft_game_result(t_init_map *data)
 		data->lenght * 40 / 2.4, data->hight * 40 / 4);
 }
 
-void check_for_errors(char **map, int hight, int lenght)
+void	check_for_errors(char **map, int hight, int lenght)
 {
-    int m;
-    m = 0;
-    while (m < hight) {
-        int l;
-        l = 0;
-        while (l < lenght) {
-            if ('C' == map[m][l] || 'E' == map[m][l]) {
-                ft_printf("Error.\nCheck path");
-                exit(EXIT_FAILURE);
-            }
-            l++;
-        }
-        m++;
-    }
+	int	m;
+	int	l;
+
+	m = 0;
+	l = 0;
+	while (m < hight)
+	{
+		l = 0;
+		while (l < lenght)
+		{
+			if ('C' == map[m][l] || 'E' == map[m][l])
+			{
+				ft_printf("Error.\nCheck path");
+				exit(EXIT_FAILURE);
+			}
+			l++;
+		}
+		m++;
+	}
 }
--- mandatory/so_long_utils2.c
+++ mandatory/so_long_utils2.c
@@ -1,70 +1,72 @@
 /* ************************************************************************** */
-/*                                                                            */
-/*                                                        :::      ::::::::   */
-/*   so_long_utils2.c                                   :+:      :+:    :+:   */
-/*                                                    +:+ +:+         +:+     */
-/*   By: csiqueir <csiqueir@student.42.fr>          +#+  +:+       +#+        */
-/*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2022/12/29 21:32:45 by csiqueir          #+#    #+#             */
-/*   Updated: 2022/12/29 21:52:51 by csiqueir         ###   ########.fr       */
-/*                                                                            */
+/*									  */
+/*							:::	 ::::::::   */
+/*   so_long_utils2.c								   :+:	  :+:	:+:   */
+/*						  +:+ +:+	   +:+	*/
+/*   By: csiqueir <csiqueir@student.42.fr>	  +#+  +:+	   +#+	*/
+/*						+#+#+#+#+#+   +#+	 */
+/*   Created: 2022/12/29 21:32:45 by csiqueir		#+#  #+#		  */
+/*   Updated: 2022/12/29 22:11:54 by csiqueir		 ###   ########.fr	   */
+/*									  */
 /* ************************************************************************** */
 
 #include "so_long.h"
 
-void free_position_queue(struct position_queue *pq)
+void	free_position_queue(struct position_queue *pq)
 {
-    free(pq->queue);
+	free(pq->queue);
 }
 
-void free_directions(int *dx, int *dy)
+void	free_directions(int *dx, int *dy)
 {
-    free(dx);
-    free(dy);
+	free(dx);
+	free(dy);
 }
 
-
-void find_player_position(char **map, int hight, int lenght, struct position *player_pos)
+void	find_player_position(char **map, int hight, int lenght,
+	struct position *player_pos)
 {
-    int i;
-    i = 0;
-	        int j;
-        j = 0;
-    while (i < hight) {
-        j = 0;
-        while (j < lenght) {
-            if (map[i][j] == 'P') {
-                break;
-            }
-            j++;
-        }
-        if (map[i][j] == 'P') {
-            break;
-        }
-        i++;
-    }
+	int	i;
+	int	j;
 
-    player_pos->x = i;
-    player_pos->y = j;
+	i = 0;
+	j = 0;
+	while (i < hight)
+	{
+		j = 0;
+		while (j < lenght)
+		{
+			if (map[i][j] == 'P')
+			{
+				break ;
+			}
+			j++;
+		}
+		if (map[i][j] == 'P')
+		{
+			break ;
+		}
+		i++;
+	}
+	player_pos->x = i;
+	player_pos->y = j;
 }
 
-void mark_visited(char **map, int i, int j)
+void	mark_visited(char **map, int i, int j)
 {
-    map[i][j] = 'V';
+	map[i][j] = 'V';
 }
 
-void initialize_directions(int **dx, int **dy)
+void	initialize_directions(int **dx, int **dy)
 {
-    *dx = malloc(sizeof(int) * 4);
-    (*dx)[0] = -1;
-    (*dx)[1] = 1;
-    (*dx)[2] = 0;
-    (*dx)[3] = 0;
-
-    *dy = malloc(sizeof(int) * 4);
-    (*dy)[0] = 0;
-    (*dy)[1] = 0;
-    (*dy)[2] = -1;
-    (*dy)[3] = 1;
+	*dx = malloc(sizeof(int) * 4);
+	(*dx)[0] = -1;
+	(*dx)[1] = 1;
+	(*dx)[2] = 0;
+	(*dx)[3] = 0;
+	*dy = malloc(sizeof(int) * 4);
+	(*dy)[0] = 0;
+	(*dy)[1] = 0;
+	(*dy)[2] = -1;
+	(*dy)[3] = 1;
 }
-
